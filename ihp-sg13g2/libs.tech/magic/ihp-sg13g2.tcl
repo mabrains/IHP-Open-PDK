390de490fffe9843bdf00de8a6e09999d05a98b6
R. Timothy Edwards <tim@opencircuitdesign.com>
2025-11-03 11:04:11 -0500

    Updated all files to contain the IHP copyright notice.  Made additional notes in the README and RELEASE_NOTES files. Changed version numbers on the technology files to 1.0.0 for official release.  There is some remaining work to be done to update to changes from IHP that are currently in pull requests but have not yet been merged into the dev branch.
    Signed-off-by: R. Timothy Edwards <tim@opencircuitdesign.com>

--- ihp-sg13g2/libs.tech/magic/ihp-sg13g2.tcl
+++ ihp-sg13g2/libs.tech/magic/ihp-sg13g2.tcl
@@ -1,10 +1,26 @@
+########################################################################
+#
+# Copyright 2025 IHP PDK Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
 #-----------------------------------------------------
 # Magic/TCL design kit for IHP ihp-sg13g2
 #-----------------------------------------------------
 # Tim Edwards
-# Revision 0.3	ALPHA   9/11/2025
-# Currently a work in progress, expected completion
-# date of January 2026.
+# Revision 1.0	November 3, 2025
 #-----------------------------------------------------
 #
 # Devices in the model files, with notes about
